0a2959106607f2671c8be70f6501d87716503bba
pholosho <35956639+pholoshos@users.noreply.github.com>
2021-10-19 11:02:41 +0200

    Revert "Added a button component called AppButton"
    This reverts commit 864ba7a4c05986276287bf373eb6399f7b9698b8.

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './pages/Login.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Login
-      
-      />
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
